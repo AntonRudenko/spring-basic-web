c25a6e23b9933e57ba34f940e43497409ed130a9
anton.rudenko <anton.rudenko@teamlabs.ru>
2015-03-25 13:40:28 +0300

    intital commit

---
+++ src/main/java/ru/teamlabs/passbook/controller/HeartBeatController.java
@@ -0,0 +1,19 @@
+package ru.teamlabs.passbook.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by dark on 25.03.15.
+ */
+@RestController
+@RequestMapping(value = "/heartbeat")
+public class HeartBeatController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String test() {
+        return "I'm alive!";
+    }
+
+}
