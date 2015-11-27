397b8c8a5ba20627b54f690614ca18a6ba6ef000
anton.rudenko <anton.rudenko@teamlabs.ru>
2015-11-27 15:38:44 +0300

    versions update

--- src/main/java/ru/teamlabs/utils/process/ProcessUtils.java
+++ src/main/java/ru/teamlabs/utils/process/ProcessUtils.java
@@ -4,7 +4,7 @@ import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.teamlabs.ProcessResult;
-import ru.teamlabs.utils.PathUtils;
+import ru.teamlabs.utils.file.PathUtils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
